5ec42fe4e19ac08e448a66d4730c931137340c93
mikheil sapharidze <portofranko36@gmail.com>
2021-10-25 22:45:28 +0200

    done

---
+++ contacts.js
@@ -0,0 +1,50 @@
+const fs = require("fs/promises");
+const path = require("path");
+const crypto = require("crypto");
+
+const contactsPath = path.join(__dirname, "./db/contacts.json");
+
+async function listContacts() {
+  const data = await fs.readFile(contactsPath);
+  return JSON.parse(data);
+}
+
+async function getContactById(contactId) {
+  const contacts = await listContacts();
+  const contact = contacts.find((item) => item.id === contactId);
+  if (!contact) {
+    return null;
+  }
+  return contact;
+}
+
+async function removeContact(contactId) {
+  const contacts = await listContacts();
+  const idx = contacts.findIndex((item) => item.id === contactId);
+  if (idx === -1) {
+    return null;
+  }
+  const removeContact = contacts.splice(idx, 1);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts));
+  return removeContact;
+}
+
+async function addContact({ name, email, phone }) {
+  const contacts = await listContacts();
+  const newContact = {
+    id: crypto.randomUUID,
+    name,
+    email,
+    phone,
+  };
+  contacts.push(newContact);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return newContact;
+}
+
+module.exports = {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+};
---
+++ index.js
@@ -0,0 +1,35 @@
+const contactsOperation = require("./contacts.js");
+const argv = require("yargs").argv;
+const chalk = require("chalk");
+async function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case "list":
+      const contacts = await contactsOperation.listContacts();
+      console.table(contacts);
+      break;
+
+    case "get":
+      const contact = await contactsOperation.getContactById(id);
+      console.table(contact);
+      break;
+
+    case "add":
+      const newContact = await contactsOperation.addContact({
+        name,
+        email,
+        phone,
+      });
+      console.table(newContact);
+      break;
+
+    case "remove":
+      const removeContact = await contactsOperation.removeContact(id);
+      console.table(removeContact);
+      break;
+
+    default:
+      console.warn(chalk.red("Unknown action type!"));
+  }
+}
+
+invokeAction(argv);
